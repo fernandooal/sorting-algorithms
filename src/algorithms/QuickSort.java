099ced62261ed12ff3b0a30d84c34452be87074e
Renato Gouveia <rpgouveia@gmail.com>
2025-11-08 02:48:41 -0300

    feat: add QuickSort class

---
+++ src/algorithms/QuickSort.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+// implementação do algoritmo de ordenação Quick Sort
+public class QuickSort implements SortingAlgorithm {
+
+    // método principal para ordenar o array usando Quick Sort
+    @Override
+    public void sort(int[] data) {
+        // TODO
+    }
+
+    // método auxiliar recursivo para o Quick Sort
+    private void quickSortRecursive(int[] data, int low, int high) {
+        // TODO
+    }
+
+    // método para particionar o array
+    private int partition(int[] data, int low, int high) {
+        // TODO
+        return 0;
+    }
+
+    // método auxiliar para trocar dois elementos no array
+    private void swap(int[] array, int i, int j) {
+        // TODO
+    }
+
+    // método para retornar o nome do algoritmo
+    @Override
+    public String getName() {
+        return "Quick Sort";
+    }
+}
