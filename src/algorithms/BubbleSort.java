a12b62de645de0426f122c15181c901e3f23d9ac
Renato Gouveia <rpgouveia@gmail.com>
2025-11-08 02:39:52 -0300

    feat: add BubbleSort class

---
+++ src/algorithms/BubbleSort.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+// implementação do algoritmo Bubble Sort
+public class BubbleSort implements SortingAlgorithm {
+
+    // método para ordenar um array usando Bubble Sort
+    @Override
+    public void sort(int[] data) {
+        // TODO
+    }
+
+    // método auxiliar para trocar dois elementos no array
+    private void swap(int[] array, int i, int j) {
+        // TODO
+    }
+
+    // método para obter o nome do algoritmo
+    @Override
+    public String getName() {
+        return "Bubble Sort";
+    }
+}
