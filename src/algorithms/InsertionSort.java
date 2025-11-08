833e115ea33854bed76a094b77ee3f591d2f80fa
Renato Gouveia <rpgouveia@gmail.com>
2025-11-08 02:43:03 -0300

    feat: add InsertionSort class

---
+++ src/algorithms/InsertionSort.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+// implementação do algoritmo Insertion Sort
+public class InsertionSort implements SortingAlgorithm {
+
+    // método para ordenar um array usando Insertion Sort
+    @Override
+    public void sort(int[] data) {
+        // TODO
+    }
+
+    // método para obter o nome do algoritmo
+    @Override
+    public String getName() {
+        return "Insertion Sort";
+    }
+}
