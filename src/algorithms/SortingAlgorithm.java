4e5c2b6ef8bf389cfc333fb70ec31c0aa3f45beb
Renato Gouveia <rpgouveia@gmail.com>
2025-11-09 01:40:16 -0300

    chore: remove comments

--- src/algorithms/SortingAlgorithm.java
+++ src/algorithms/SortingAlgorithm.java
@@ -1,11 +1,8 @@
 package algorithms;
 
-// interface para algoritmos de ordenação
 public interface SortingAlgorithm {
 
-    // método para ordenar um array de inteiros
     void sort(int[] data);
 
-    // método para obter o nome do algoritmo
     String getName();
 }
