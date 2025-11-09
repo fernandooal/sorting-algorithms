73093ef3807b60e4368d1092378ef0a991e71129
fernandooal <fernandoalonsops@hotmail.com>
2025-11-08 23:41:45 -0300

    feat: quicksort + sorting example in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import algorithms.SortingAlgorithm;
+import algorithms.QuickSort;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] data = new int[]{7, 1, 3, 5, 2, 6, 4};
+
+        System.out.println("Array original: " + Arrays.toString(data));
+
+        SortingAlgorithm quickSort = new QuickSort();
+        quickSort.sort(data);
+
+        System.out.println("Array ordenado: " + Arrays.toString(data));
     }
 }
